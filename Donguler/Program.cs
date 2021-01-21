97d409e714bba8e198000d4d2a37d9318cf6b082
Murat Çay <muratthebestof@gmail.com>
2021-01-21 13:00:08 +0300

    16 Ocak Ders Sonu

---
+++ Donguler/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici...";
+            string kurs2 = "Programlamaya başlangıc";
+            string kurs3 = "Java";
+            string kurs4 = "Python";
+            
+
+            string[] kurslar = new string[] {kurs1,kurs2,kurs3,kurs4 };
+
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            Console.WriteLine("for bitti !");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+        }
+    }
+}
---
+++ KampIntro/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategori";
+            Console.WriteLine(kategoriEtiketi);
+
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = false;
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Buttonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi butonu");
+            }
+
+
+
+        }
+    }
+}
